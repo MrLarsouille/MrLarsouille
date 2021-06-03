3d0b13cd446950a8785052b7e168f3f3d84746d8
Larsouille <bastiensachot0@gmail.com>
2021-06-03 17:24:49 +0200

    initial revision
    add test files

---
+++ Exercices/Version1.py
@@ -0,0 +1,4 @@
+x=12
+resultat=x+5
+resultat=resultat*3
+print(resultat)
---
+++ Exercices/calcul.py
@@ -0,0 +1,10 @@
+x=45
+resultat=x-10
+resultat=resultat*10
+print(resultat)
+
+
+print("Welcome to mathematics")
+45+10
+resultat=45+10
+print("Well done the result is" +repr (resultat)+" !")
